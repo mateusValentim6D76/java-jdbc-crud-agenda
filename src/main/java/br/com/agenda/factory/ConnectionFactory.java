13c2690eb7cb3f115e67f53dff09089c120a6f5c
Mateus Valentim de Oliveira <mateus.oliveira@keeggo.com>
2021-10-28 11:34:05 -0300

    Verificando se já há uma conexão ativa - Pattern Singleton

--- src/main/java/br/com/agenda/factory/ConnectionFactory.java
+++ src/main/java/br/com/agenda/factory/ConnectionFactory.java
@@ -9,17 +9,28 @@ public class ConnectionFactory {
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "";
 	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenda";
-	
+
 	/*
 	 * Conexão com o banco
 	 */
-	public static Connection createConnectionToMySql() throws Exception  {
-		//Faz com que a classe seja carregada pela JVM
+	public static Connection createConnectionToMySql() throws Exception {
+		// Faz com que a classe seja carregada pela JVM
 		Class.forName("com.mysql.jdbc.Driver");
-		
-		//Cria a conexão com o Banco de Dados
+
+		// Cria a conexão com o Banco de Dados
 		Connection connection = DriverManager.getConnection(USERNAME, PASSWORD, DATABASE_URL);
-		
+
 		return connection;
+	}
+
+	public static void main(String[] args) throws Exception {
+		 //Recuperar conexão com o banco de dados
+		Connection conn = createConnectionToMySql();
+		
+		//Testa se a conexão é nula
+		if(conn !=null) {
+			System.out.println("Conexão obtida com sucesso");
+			conn.close();
+		}
 	}
 }
